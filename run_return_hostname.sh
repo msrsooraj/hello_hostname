aaf255364f1a7e889d0cf7fb03ca97234b5eacb9
Sooraj <msrsooraj@hotmail.com>
2020-07-15 19:34:17 +0530

    Update run_return_hostname.sh

--- run_return_hostname.sh
+++ run_return_hostname.sh
@@ -1,8 +1,7 @@
 sudo apt-get update
 sudo apt install -y python-pip
-sudo pip install --upgrade pip
 sudo pip install Flask
 
-wget -O return_hostname.py https://raw.githubusercontent.com/msrsooraj/lazyscripts/master/return_hostname.py
+wget https://raw.githubusercontent.com/msrsooraj/lazyscripts/master/return_hostname.py
 
 sudo nohup python return_hostname.py &>/dev/null &disown
