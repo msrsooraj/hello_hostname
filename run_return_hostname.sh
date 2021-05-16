25f34cdd422d65ab3a057d47e8eb42fa57183007
Sooraj <msrsooraj@hotmail.com>
2021-05-17 03:20:09 +0530

    Update run_return_hostname.sh

--- run_return_hostname.sh
+++ run_return_hostname.sh
@@ -5,3 +5,7 @@ sudo pip3 install Flask
 wget https://raw.githubusercontent.com/msrsooraj/lazyscripts/master/return_hostname.py
 
 sudo python3 return_hostname.py &> return_hostname.log &
+
+echo "Flask Server with Hello Hostname has been started."
+echo "Hitting the IP address or the DNS Name will return the hostname. Use the loadbalancer name is the VM is under a loadbalancer."
+echo 'Logfile: return_hostname.log.'
